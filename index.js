c06351a0b762a6421c6055a1d4d87d971d786b21
Sudsun12 <kacemdabbou123@gmail.com>
2024-11-29 22:53:43 +0100

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Serves files from the root directory
+app.use(express.static('./'));
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
